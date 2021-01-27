1afbffa25774ae602756178d4bbf12bd74af9972
Ievhen <bilionjenia1111@gmail.com>
2021-01-27 09:47:40 +0200

    chanded code style

--- src/api/request.js
+++ src/api/request.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 import { CURRENCY_URL } from "./constants";
 
-export const fetchData = async(currencyFrom) => {
+export const fetchData = async (currencyFrom) => {
   try {
-    const response = await axios
-      .get(`${CURRENCY_URL}latest?base=${currencyFrom}`)
+    const response = await axios.get(
+      `${CURRENCY_URL}latest?base=${currencyFrom}`
+    );
     return response.data;
   } catch (error) {
     console.error(error);
   }
-  
-}
-
+};
