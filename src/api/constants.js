08d4fb40ede00c75b5f6d8409fe7b02aa0bb0f3c
Ievhen <bilionjenia1111@gmail.com>
2021-07-30 14:54:22 +0300

    fixed currency api

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,2 +1,2 @@
-export const CURRENCY_URL = 'https://api.exchangeratesapi.io/v1/'
+export const CURRENCY_URL = 'http://api.exchangeratesapi.io/v1/latest'
 export const ACCESS_KEY = 'd7be9d01b04e9c91d1500e9172f24af7'
--- src/api/request.js
+++ src/api/request.js
@@ -1,17 +1,16 @@
 import axios from "axios";
+import { ACCESS_KEY, CURRENCY_URL } from "./constants";
 
 export const fetchData = async (currencyFrom) => {
   try {
-  //   const params = {
-  //     access_key: ACCESS_KEY,
-  //     format: 1
-  //  }
+    const params = {
+      access_key: ACCESS_KEY,
+      format: 1
+   }
     const response = await axios.get(
-      `http://api.exchangeratesapi.io/v1/latest?access_key=d7be9d01b04e9c91d1500e9172f24af7&format=1`
+      CURRENCY_URL,{params}
     );
-    console.log('currencyFrom',currencyFrom);
     const resp = response.data.rates[currencyFrom]
-    console.log('resp', resp);
     return resp;
   } catch (error) {
     console.error(error);
